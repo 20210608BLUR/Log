5e59c13a6b29896cb0bcea0129bc81b3a5914484
DINGDING <mumu5252@yahoo.com.tw>
2023-06-16 16:53:09 +0800

    加入專案檔案。

---
+++ Dev_Log/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Dev_Log
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileOpenBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.fileReadBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(16, 361);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(600, 224);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "清單顯示";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(12, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "詳細資訊";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(16, 183);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(600, 123);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 21);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "輸入檔案來源";
+            // 
+            // fileOpenBtn
+            // 
+            this.fileOpenBtn.BackColor = System.Drawing.Color.Gainsboro;
+            this.fileOpenBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.fileOpenBtn.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.fileOpenBtn.Location = new System.Drawing.Point(134, 85);
+            this.fileOpenBtn.Name = "fileOpenBtn";
+            this.fileOpenBtn.Size = new System.Drawing.Size(328, 32);
+            this.fileOpenBtn.TabIndex = 12;
+            this.fileOpenBtn.Text = "本機檔案";
+            this.fileOpenBtn.UseVisualStyleBackColor = false;
+            this.fileOpenBtn.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(136, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 29);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Tag = "";
+            // 
+            // fileReadBtn
+            // 
+            this.fileReadBtn.BackColor = System.Drawing.Color.Gainsboro;
+            this.fileReadBtn.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.fileReadBtn.ForeColor = System.Drawing.Color.Gray;
+            this.fileReadBtn.Location = new System.Drawing.Point(488, 31);
+            this.fileReadBtn.Name = "fileReadBtn";
+            this.fileReadBtn.Size = new System.Drawing.Size(128, 86);
+            this.fileReadBtn.TabIndex = 14;
+            this.fileReadBtn.Text = "確定";
+            this.fileReadBtn.UseVisualStyleBackColor = false;
+            this.fileReadBtn.Click += new System.EventHandler(this.fileRead_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 21);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "選擇檔案來源";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 24);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "匯出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 624);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fileReadBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fileOpenBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Log";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fileOpenBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button fileReadBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Dev_Log/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Configuration;
+
+namespace Dev_Log
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> dicResults = new Dictionary<string, string>();
+        string[] errList;
+        string file = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = dicResults[listBox1.SelectedItem.ToString()];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // 開啟檔案
+        private void fileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "(*.log)|*.log|(*.txt*)|*.txt*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = openFileDialog.FileName;
+                textBox1.Text = file.ToString();
+            }
+        }
+
+        // 檢視 log 錯誤清單
+        private void fileRead_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+
+            // 網址
+            if (textBox1.Text.Contains("https") || textBox1.Text.Contains("http"))
+            {
+                try
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        if (textBox1.Text != null)
+                        {
+                            Stream stream = wc.OpenRead(textBox1.Text);
+                            StreamReader sr = new StreamReader(stream);
+
+                            string log = sr.ReadToEnd();
+                            string[] data = log.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+
+                            if (listBox1.Items.Count > 0)
+                            {
+                                listBox1.Items.Clear();
+                            }
+
+                            dicResults.Clear();
+
+                            foreach (var d in data)
+                            {
+                                if (d.Contains("類型：ERR") == true)
+                                {
+                                    errList = d.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                                    dicResults.Add(errList[1] + " / " + errList[5], d);
+                                    listBox1.Items.Add(errList[1] + " / " + errList[5]);
+                                }
+                            }
+                            sr.Dispose();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (listBox1.Items.Count > 0)
+                    {
+                        listBox1.Items.Clear();
+                    }
+
+                    MessageBox.Show("查無資料");
+                }
+            }
+            // 本機
+            else
+            {
+                try
+                {
+                    richTextBox1.Text = string.Empty;
+
+                    StreamReader sr = new StreamReader(file);
+
+                    string log = sr.ReadToEnd();
+                    string[] data = log.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None);
+
+                    if (listBox1.Items.Count > 0)
+                    {
+                        listBox1.Items.Clear();
+                    }
+
+                    dicResults.Clear();
+
+                    foreach (var d in data)
+                    {
+                        if (d.Contains("類型：ERR") == true)
+                        {
+                            errList = d.Split(new string[] { "\r\n" }, StringSplitOptions.None);                                                       
+                            dicResults.Add(errList[1] + " / " + errList[5], d);
+                            listBox1.Items.Add(errList[1] + " / " + errList[5]);                            
+                        }
+                    }
+                    sr.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    if (listBox1.Items.Count > 0)
+                    {
+                        listBox1.Items.Clear();
+                    }
+
+                    MessageBox.Show("查無資料");
+                }
+            }
+        }
+
+        // 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"E:\\.txt";
+            string[] array = new string[listBox1.Items.Count];
+            int i = 0;
+            
+            foreach(var lt in listBox1.Items)
+            {
+                string list = lt.ToString();
+                array[i++] = list;
+            };           
+
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+
+                //foreach(var txt in array)
+                //{
+                //    string text  = dicResults[txt.ToString()];
+                //    sw.WriteLine(text);
+                //    sw.Write("\r\n");
+                //}
+
+                for (int j = 0;j < listBox1.Items.Count; j++)
+                {
+                    sw.Write(dicResults[listBox1.Items[j].ToString()]);
+                    sw.Write("\r\n");
+                }
+
+                sw.Close();
+                fs.Close();
+
+                MessageBox.Show("匯出成功");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("匯出失敗");
+            }
+            
+        }
+    }
+}
+
